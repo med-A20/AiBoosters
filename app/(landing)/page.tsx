e64d0013f1520060dd0810ee462c6af608f43052
Amchia_Mohamed <amchiamohamed41@gmail.com>
2023-08-15 15:48:39 +0100

    changes

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,9 +1,17 @@
+"use client"
 import LandingHero from '@/components/LandingHero'
 import LandingNavbar from '@/components/LandingNavbar'
 import { Button } from '@/components/ui/button'
 import Link from 'next/link'
+import {useAuth} from '@clerk/nextjs'
+import {useRouter} from 'next/navigation'
 
 export default function Home() {
+  const {isSignedIn}  = useAuth();
+  const router = useRouter()
+  if(isSignedIn){
+   router.push("/dashboard")
+  }
   return (
     <div>
       {/* Nav Bar */}
