b8a710008ecec584dff4010bacb01dc1629ceac3
Amchia_Mohamed <amchiamohamed41@gmail.com>
2023-08-18 11:14:52 +0100

    fix clerk

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,17 +1,18 @@
 "use client"
 import LandingHero from '@/components/LandingHero'
 import LandingNavbar from '@/components/LandingNavbar'
-import { Button } from '@/components/ui/button'
-import Link from 'next/link'
 import {useAuth} from '@clerk/nextjs'
 import {useRouter} from 'next/navigation'
+import { useEffect } from 'react'
 
 export default function Home() {
   const {isSignedIn}  = useAuth();
   const router = useRouter()
-  if(isSignedIn){
-   router.push("/dashboard")
-  }
+  useEffect(() => {
+    if (isSignedIn) {
+      router.push('/dashboard');
+    }
+  }, [isSignedIn]);
   return (
     <div>
       {/* Nav Bar */}
