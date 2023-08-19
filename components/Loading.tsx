a849dfa95fb442d7a92e65ace7f46fe85268f2da
Amchia_Mohamed <amchiamohamed41@gmail.com>
2023-08-19 23:32:09 +0100

    fix bugs ..

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Loading = () => {
   return (
-    <div className='w-screen m-auto md:min-h-[5rem] md:min-w-[6rem] flex flex-col justify-center items-center'>
+    <div className='m-auto min-h-[5rem] min-w-[6rem] flex flex-col justify-center items-center'>
       <div className='relative w-20 h-20 border-b-2 border-l-2 border-rose-400 rounded-full animate-spin'>
         {/* <Image 
           src="/logo.png"
